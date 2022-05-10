651521b1e8cc45db5bf26d188500016a7e3d7639
Tran Duc Binh <binhtran432k@gmail.com>
2022-05-10 09:39:49 +0700

    add image panel with status bar

--- src/app/src/main/java/com/github/plantumlpreviewer/MainWindow.java
+++ src/app/src/main/java/com/github/plantumlpreviewer/MainWindow.java
@@ -1,16 +1,21 @@
 package com.github.plantumlpreviewer;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * Main window of application
  *
  * Please see the {@link javax.swing.JFrame} for true identity
+ * 
  * @author Tran Duc Binh
  * @since 1.0.0
  *
  */
 public class MainWindow extends JFrame {
+
+    private ImagePanel imagePanel = new ImagePanel();
+    private JPanel currentPanel;
 
     public MainWindow() {
         initUI();
@@ -21,6 +26,23 @@ public class MainWindow extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
         setLocationRelativeTo(null); // always startup center
+
+        switchToPanel(imagePanel);
+    }
+
+    private void switchToPanel(JPanel toPanel) {
+        if (currentPanel == toPanel) {
+            return;
+        }
+
+        if (currentPanel != null) {
+            remove(currentPanel);
+        }
+        currentPanel = toPanel;
+        add(currentPanel);
+
+        revalidate();
+        repaint();
     }
 
 }
