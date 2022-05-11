57146a1afcbb26dac5b0f36ad4053315b408d151
Tran Duc Binh <binhtran432k@gmail.com>
2022-05-11 11:03:07 +0700

    add plantuml dep to gralde

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
+
+    // https://mvnrepository.com/artifact/net.sourceforge.plantuml/plantuml
+    implementation("net.sourceforge.plantuml:plantuml:1.2022.5")
 }
 
 application {
